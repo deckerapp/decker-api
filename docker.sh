36d355806fcc285d2a7b402e916dce3314a117e8
Vincent <vincentbusiness55@gmail.com>
2022-07-31 10:54:20 +0800

    fix: unpublicize port in docker builds
    Signed-off-by: Vincent <vincentbusiness55@gmail.com>

--- docker.sh
+++ docker.sh
@@ -2,4 +2,4 @@
 sudo docker build -t derailed-api .
 sudo docker stop derailed-api
 sudo docker rm -v derailed-api
-sudo docker run --name derailed-api -d -p 5000:5000 derailed-api
+sudo docker run --name derailed-api -d derailed-api
