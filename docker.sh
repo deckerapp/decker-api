2f94c143959033beee681b470ca833d367a290b3
Vincent <vincentbusiness55@gmail.com>
2022-07-31 13:23:08 +0800

    fix: revert port removal
    Signed-off-by: Vincent <vincentbusiness55@gmail.com>

--- docker.sh
+++ docker.sh
@@ -2,4 +2,4 @@
 sudo docker build -t derailed-api .
 sudo docker stop derailed-api
 sudo docker rm -v derailed-api
-sudo docker run --name derailed-api -d derailed-api
+sudo docker run --name derailed-api -p 5000:5000 -d derailed-api
